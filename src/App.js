a68f16ab44cc1bd1d5df40703295fb34e4cc5731
jef1993 <jeffrey24493@gmail.com>
2021-08-27 19:41:16 +0100

    v 1.0

--- src/App.js
+++ src/App.js
@@ -1,34 +1,47 @@
 import React from "react";
+import { useState } from "react";
+import { evaluate } from "mathjs";
 import Button, { layout } from "./Button";
 
 function App() {
+  const [str, setStr] = useState("");
+  const [output, setOutput] = useState("");
+
+  const handleClick = (e) => {
+    const symbol = e.target.innerHTML;
+
+    if (symbol === "C") {
+      setStr(str.slice(0, -1));
+    } else if (symbol === "DC") {
+      setStr("");
+    } else if (symbol === "=") {
+      if (!str.match(/\D\D/) && !str[0].match(/[*/]/))
+        setStr(evaluate(str).toString());
+      setOutput(evaluate(str).toString());
+    } else {
+      setStr(str + symbol);
+    }
+  };
+
   return (
     <div className="App">
       <div className="main">
         <div className="monitor">
-          <div className="monitor__content">content</div>
+          <div className="monitor__input">{str}</div>
+          <div className="monitor__output">{output}</div>
         </div>
 
         <div className="buttons">
-          <Button class="" content="C" />
-          <Button class="" content="DC" />
-          <Button class="" content="/" />
-          <Button class="" content="*" />
-          <Button class="" content="7" />
-          <Button class="" content="8" />
-          <Button class="" content="9" />
-          <Button class="" content="-" />
-          <Button class="" content="4" />
-          <Button class="" content="5" />
-          <Button class="" content="6" />
-          <Button class="" content="+" />
-          <Button class="" content="1" />
-          <Button class="" content="2" />
-          <Button class="" content="3" />
-          <Button class="equal" content="=" />
-          <Button class="num-0" content="0" />
-          <Button class="" content="." />
-
+          {layout.map((el, i) => {
+            return (
+              <Button
+                class={el.name}
+                content={el.symbol}
+                click={handleClick}
+                key={i}
+              />
+            );
+          })}
         </div>
       </div>
     </div>
